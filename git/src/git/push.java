d6eabc2f41589ddbd5aa208ab0b07136bdf37826
GihongPark <34112859+GihongPark@users.noreply.github.com>
2018-04-26 21:14:09 +0900

    pull

--- git/src/git/push.java
+++
@@ -1,9 +0,0 @@
-package git;
-
-public class push {
-	public static void main(String[] args) {
-		System.out.println("add java class");
-		System.out.println("java changed");
-		System.out.println("web changed");
-	}
-}
